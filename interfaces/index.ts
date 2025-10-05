939f3f5751667b46b77237591d9b5ad2be641946
Elleni-Bisrat <ellenibisrat16@gmail.com>
2025-10-05 21:40:30 +0300

    Property detail page

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,18 +1,27 @@
-export interface PropertyProps{
-    name:string;
-    address:{
-        state:string;
-        city:string;
-        country:string;
-    };
-    rating:number;
-    category:string[];
-    price:number;
-    offers:{
-        bed:string;
-        shower:string;
-        occupants:string;
-    };
-    image:string;
-    discount:string;
+export interface ReviewProps {
+  name: string;
+  avatar: string;
+  rating: number;
+  comment: string;
+}
+
+export interface PropertyProps {
+  name: string;
+  address: {
+    state: string;
+    city: string;
+    country: string;
+  };
+  rating: number;
+  category: string[];
+  price: number;
+  offers: {
+    bed: string;
+    shower: string;
+    occupants: string;
+  };
+  image: string;
+  discount: string;
+  description?: string;
+  reviews?: ReviewProps[];
 }
